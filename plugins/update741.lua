fc1db29b03122db9f92e3f14df6531a8fb1c67f0
publitabc <gdanya2nd@gmail.com>
2022-05-17 18:00:29 +0300

    Update update741.lua

--- plugins/update741.lua
+++ plugins/update741.lua
@@ -1,2 +1,2 @@
-if abc.version < 800 then abc.tool.local_settings("global_settings_background_file", "\\\\styles\\\\default\\\\back2.png"); end
+abc.tool.local_settings("global_settings_background_file", "\\\\styles\\\\default\\\\back2.png");
 File.Delete(_SourceFolder.."\\plugins\\update741.lua");
