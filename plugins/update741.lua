f6de37b6caac87901ff6415090c549cff3f3006c
publitabc <gdanya2nd@gmail.com>
2022-05-17 17:40:47 +0300

    v881
    https://www.patreon.com/posts/66449254

--- plugins/update741.lua
+++ plugins/update741.lua
@@ -1,2 +1,2 @@
-abc.tool.local_settings("global_settings_background_file", "\\\\styles\\\\default\\\\back2.png");
+if abc.version < 800 then abc.tool.local_settings("global_settings_background_file", "\\\\styles\\\\default\\\\back2.png"); end
 File.Delete(_SourceFolder.."\\plugins\\update741.lua");
