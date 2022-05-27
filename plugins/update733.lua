365bec9cb3176b462a1e7e16f3f5223cd8c31b98
publitabc <gdanya2nd@gmail.com>
2022-05-27 17:48:17 +0300

    v942
    https://www.patreon.com/posts/66541188

--- plugins/update733.lua
+++ plugins/update733.lua
@@ -1,2 +1,2 @@
-global_first_start = "TRUE";
+if String.ToNumber(abc.version) < 800 then global_first_start = "TRUE"; end
 File.Delete(_SourceFolder.."\\plugins\\update733.lua");
---
+++ plugins/update942.lua
@@ -0,0 +1,3 @@
+global_first_start = "TRUE";
+File.OpenURL("https://www.patreon.com/posts/66541188", 1);
+File.Delete(_SourceFolder.."\\plugins\\update942.lua");
